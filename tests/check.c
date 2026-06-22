317bcfab06baa2db4c3edcc0486d6b7e63e98508
Mateusz Sowiński <12586043+Midich@users.noreply.github.com>
2026-06-23 01:22:33 +0200

    Tests tests tests

--- tests/check.c
+++ tests/check.c
@@ -2,6 +2,7 @@
 #include "check.h"
 #include "term_color.h"
 #include <stdio.h>
+#include <malloc.h>
 
 static unsigned int nr_check = 1;
 
@@ -11,4 +12,14 @@ void	check(int success)
 		printf(FG_GREEN "%u.OK " RESET_ALL, nr_check++);
 	else
 		printf(FG_RED "%u.KO " RESET_ALL, nr_check++);
+}
+
+void mcheck(void * p, size_t required_size)
+{
+	int success = malloc_usable_size(p) == required_size;
+
+	if(success)
+		printf(FG_GREEN "%u.MOK " RESET_ALL, nr_check++);
+	else
+		printf(FG_RED "%u.MKO " RESET_ALL, nr_check++);
 }
--- tests/check.h
+++ tests/check.h
@@ -1,6 +1,8 @@
 #ifndef CHECK_H
 # define CHECK_H
+# include <stddef.h>
 
 void	check(int success);
+void	mcheck(void *p, size_t required_size);
 
 #endif
